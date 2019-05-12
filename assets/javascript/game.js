86e01587948f962d190b45833bc9fb0ca32bab52
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-12 09:49:02 -0400

    psuedo code and initial CSS

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// create random computer number to be placed as the random number for the computer and then used for each jewel click
+
+// adds random number from jewel clicks.  takes two paramaters
+
+//checks if player has won
+
+// checks if player has lost
+
+// new game - resets random numbers and sets user score to 0
+
+
