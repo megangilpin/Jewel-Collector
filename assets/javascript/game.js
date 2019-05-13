b6168ab49506bdfc17b73e5d0516e95c84936a43
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-13 06:27:40 -0400

    ranNumArr created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,30 @@
-// create random computer number to be placed as the random number for the computer and then used for each jewel click
+
+// creates an array of 6 random numbers to be used for the random comp # and 4 jewel numbers
+
+function ranNumGenorator(arr) {
+    var ranNumArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        var random = Math.floor(Math.random() * 100);
+        ranNumArr.push(random)
+    }
+    console.log(lottoNumber)
+    return lottoNumber
+}
+
+// function ranNumGenorator(arr){
+//     varrandNum = Math.floor(Math.random() * 1000) + 1;
+//     arr.splice(randNum);
+//     return arr;
+
+// var numArr = [1,2,3,4,5]
+// function ranNumGenorator(arr){
+//     var randomNumArr = [];
+//     for (i = 0; i < arr.length; i++){
+//     randomNumArr[i] = Math.floor(Math.random() * 100) + 1;
+//     }
+//     return randomNumArr;
+// };
+
 
 // adds random number from jewel clicks.  takes two paramaters
 
