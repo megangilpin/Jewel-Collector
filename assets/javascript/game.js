ed0d5bc9222e2e3887219f0ab9e402685256ed3c
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-13 07:01:17 -0400

    comp number function fix ranNumGenorator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,27 @@
+// Arrya of number to be assigned to comp and jewels
+var numArr = [1, 2, 3, 4]
+
+// Creates random computer number from 120 -19
+$("#compnum").text(function () {
+    return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+});
 
 // creates an array of 6 random numbers to be used for the random comp # and 4 jewel numbers
-
 function ranNumGenorator(arr) {
     var ranNumArr = [];
     for (var i = 0; i < arr.length; i++) {
-        var random = Math.floor(Math.random() * 100);
+        var random = Math.floor(Math.random() * 12)+1;
         ranNumArr.push(random)
     }
-    console.log(lottoNumber)
-    return lottoNumber
+    console.log(ranNumArr)
+    return ranNumArr
 }
-
-// function ranNumGenorator(arr){
-//     varrandNum = Math.floor(Math.random() * 1000) + 1;
-//     arr.splice(randNum);
-//     return arr;
-
-// var numArr = [1,2,3,4,5]
-// function ranNumGenorator(arr){
-//     var randomNumArr = [];
-//     for (i = 0; i < arr.length; i++){
-//     randomNumArr[i] = Math.floor(Math.random() * 100) + 1;
-//     }
-//     return randomNumArr;
-// };
 
 
 // adds random number from jewel clicks.  takes two paramaters
+
+
+
 
 //checks if player has won
 
