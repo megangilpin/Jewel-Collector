855d825084a62c11085ce9db9ffbf795f7f9e5e5
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-14 01:00:32 -0400

    random num functions and assigns val

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,81 @@
-// Arrya of number to be assigned to comp and jewels
-var numArr = [1, 2, 3, 4]
+// Array of id's for random numbers to be assigned to
+var jewelArr = ["#bluejewel", "#greenjewel", "#pinkjewel", "#purplejewel"]
 
-// Creates random computer number from 120 -19
-$("#compnum").text(function () {
-    return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-});
+var compRanNum = largeRanNum()
 
-// creates an array of 6 random numbers to be used for the random comp # and 4 jewel numbers
-function ranNumGenorator(arr) {
-    var ranNumArr = [];
-    for (var i = 0; i < arr.length; i++) {
-        var random = Math.floor(Math.random() * 12)+1;
-        ranNumArr.push(random)
-    }
-    console.log(ranNumArr)
-    return ranNumArr
+var bttnRanNum = smallRanNum()
+
+// creates an object that sets up all components of the game
+function setupGame(num1, num2){
+  var game = {
+    losses:0,
+    wins: 0,
+    compNum: num1, //will call the function that assigns the random number from the large num function
+    playerNum: 0,
+    bttnNums: num2, //will call the function that assigns the random number values to the button id's
+  };
+  return game
 }
 
+// Creates random number from 19-120 to be used as the computer's random number
+function largeRanNum(){
+    return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+}
 
-// adds random number from jewel clicks.  takes two paramaters
+// Creates random number from 19-120 to be used as the computer's random number
+function smallRanNum() {
+    return Math.floor(Math.random() * 12) + 1;
+}
 
+// Assigns an array of id's used on each jewel to a random number
+function jewelGenerator(arr, num) {
+    for (var i = 0; i < arr.length; i++) {
+        $(arr[i]).val(num);
+        // $(arr[i]).append(jewel)
+console.log(arr[i].val(num))
+}
+ 
+
+// writes larger random number to the compnum div
+// $("#compnum").text(function () {
+//     return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+// });
+
+// creates an array of 4 random numbers to be used for the 4 jewel numbers
+// function ranNumArr(arr) {
+//     var ranNumArr = []
+//     for (var i = 0; i < arr.length; i++) {
+//         var random = Math.floor(Math.random() * 12)+1;
+//         ranNumArr.push(random)
+//     }
+//     console.log(ranNumArr)
+//     return ranNumArr
+// };
+
+
+// Assigns an array of id's used on each jewel to a random number
+
+// function jewelGenerator(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         var jewel = $("<span>");
+//         jewel.attr("random-num", Math.floor(Math.random() * 12) + 1);
+//         // $("#"+arr[0]+"jewel").append(jewel)
+//         $(arr[i]).append(jewel)
+// console.log(jewel)
+// $('#app').val()
+    }
+// jewel.attr("random-num", ranArr[0]);
+// console.log("jewel: ",jewel);
+// }
+
+
+jewelGenerator(jewelArr)
+
+// adds jewel's random number attribut to the playerNum
+$(".bttn").on("click", function () {
+    num += $(this).val();
+    $("#player-num").text(num);
+});
 
 
 
