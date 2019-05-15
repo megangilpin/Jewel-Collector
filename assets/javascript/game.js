d07ed4f84baa7d99b074aea3777397d47629efe1
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-14 21:56:57 -0400

    created variable round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,26 @@
 // Array of id's for random numbers to be assigned to
 var jewelArr = ["#blue-jewel", "#green-jewel", "#pink-jewel", "#purple-jewel"]
 
-// var bttnRanNum = smallRanNum()
-
 // creates an object that sets up all components of the game
-function setupGame(num1, num2){
+function setupGame(arr){
   var game = {
     losses:0,
     wins: 0,
-    compNum: num1, //will call the function that assigns the random number from the large num function
+    compNum: largeRanNum(),
     playerNum: 0,
-    bttnNum: num2, //will call the function that assigns the random number values to the button id's
+    bttnNum: jewelGenerator(arr), 
   };
   return game
 }
 
 // Creates random number from 19-120 to be used as the computer's random number
 function largeRanNum(){
-    var compRanNum =  Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    return compRanNum;
+    return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 }
 
 // Creates random number from 19-120 to be used as the computer's random number
 function smallRanNum() {
-    var ranNum =  Math.floor(Math.random() * 12) + 1;
-    return ranNum;
+    return Math.floor(Math.random() * 12) + 1;
 }
 
 // Assigns an array of id's used on each jewel to a random number
@@ -34,8 +30,36 @@ function jewelGenerator(arr) {
     }
 }
 
-jewelGenerator(jewelArr)
-console.log(jewelGenerator(jewelArr))
+// adds jewel's random number attribute to the playerNum
+
+var round = setupGame(jewelArr)
+console.log(round)
+
+$("#comp-num").text(round.compNum)
+
+function playerScore(game){
+    game.playerNum += $(this).attr;
+    console.log(game.playerNum)
+    return game.playerNum;
+}
+
+$(".bttn").on("click", function () {
+    var jewelValue = ($(this).attr("jewel-number"));
+    jewelValue = parseInt(jewelValue);
+
+    round.playerNum +=
+  
+    playerScore(game);
+    $("#player-num").text(game.playerNum);
+});
+
+//checks if player has won
+
+// checks if player has lost
+
+// new game - resets random numbers and sets user score to 0
+
+
 
 
 // writes larger random number to the comp-num div
@@ -55,36 +79,13 @@ console.log(jewelGenerator(jewelArr))
 // };
 
 
-// Assigns an array of id's used on each jewel to a random number
-
-// function jewelGenerator(arr) {
-//     for (var i = 0; i < arr.length; i++) {
-//         var jewel = $("<span>");
-//         jewel.attr("random-num", Math.floor(Math.random() * 12) + 1);
-//         // $("#"+arr[0]+"jewel").append(jewel)
-//         $(arr[i]).append(jewel)
-// console.log(jewel)
-// $('#app').val()
-    // }
-// jewel.attr("random-num", ranArr[0]);
-// console.log("jewel: ",jewel);
+// function setupGame(arr) {
+//     var game = {
+//         losses: 0,
+//         wins: 0,
+//         compNum: largeRanNum(), //will call the function that assigns the random number from the large num function
+//         playerNum: 0,
+//         bttnNum: jewelGenerator(arr), //will call the function that assigns the random number values to the button id's
+//     };
+//     return game
 // }
-
-
-// jewelGenerator(jewelArr)
-
-// adds jewel's random number attribute to the playerNum
-$(".bttn").on("click", function () {
-    num += $(this).val();
-    $("#player-num").text(num);
-});
-
-
-
-//checks if player has won
-
-// checks if player has lost
-
-// new game - resets random numbers and sets user score to 0
-
-
