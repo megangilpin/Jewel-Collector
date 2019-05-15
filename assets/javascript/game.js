cc6061de15e28611dff30200de9c96d1820d8365
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-15 07:30:58 -0400

    complete game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,23 @@
 var jewelArr = ["#blue-jewel", "#green-jewel", "#pink-jewel", "#purple-jewel"]
 
 
-// creates object of game attributes
-    var round = {
-        wins: 0,
-        losses: 0,
-        compNum: largeRanNum(),
-        playerNum: 0,
-        bttnNum: jewelGenerator(jewelArr),
-    };
+// game attributes
+var wins = 0;
+var losses = 0;
+var compNum = 0;
+var playerNum = 0;
 
-
-// sets up game
-function setupGame(arr){
-  var newGame = 
-    bttnNum = jewelGenerator(arr);
+// sets up all the game attributes
+function setupGame(arr) {
     compNum = largeRanNum();
-    playerNum = playerNum;
-  }
+    playerNum = 0;
+    jewelGenerator(arr);
+    $("#comp-num").text(compNum);
+    $("#player-num").text(playerNum);
+}
 
 // Creates random number from 19-120 to be used as the computer's random number
-function largeRanNum(){
+function largeRanNum() {
     return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 }
 
@@ -37,70 +34,35 @@ function jewelGenerator(arr) {
     }
 }
 
-$("#comp-num").text(round.compNum)
-
-$("#wins").text(round.wins)
-
-$("#losses").text(round.losses)
-
-$("#player-num").text(round.playerNum)
-
-
 // On jewel click changes attribute(always a string) into an integer then adds the integer to the player score then checks if player has won or lost
 $(".bttn").on("click", function () {
     var jewelValue = ($(this).attr("jewel-number"));
     jewelValue = parseInt(jewelValue);
 
-    round.playerNum += jewelValue;
+    playerNum += jewelValue;
 
-    $("#player-num").text(round.playerNum);
+    $("#player-num").text(playerNum);
 
-    // $("#")
-
-    if (round.playerNum === round.compNum) {
-        round.wins += 1;
-        $("#wins").text(round.wins);
+    // checks if playerNum equals compNum if true adds to wins counter and alerts player they won
+    if (playerNum === compNum) {
+        wins += 1;
+        $("#wins").text(wins);
         alert("You win all the Jewels");
-        round.compNum = largeRanNum();
-        round.bttnNum = jewelGenerator(jewelArr);
+        setupGame(jewelArr);
     }
 
-    else if (round.playerNum >= round.compNum) {
-        round.losses += 1;
-        $("#losses").text(round.losses);
+    // checks if playerNum is bigger than compNum if true adds to losses counter and alerts player they lost
+    else if (playerNum >= compNum) {
+        losses += 1;
+        $("#losses").text(losses);
         alert("You lose! No Jewels for you!");
+        setupGame(jewelArr);
     }
-
 });
 
-// new game - resets random numbers and sets user score to 
 $("#new-game").on("click", function () {
-    var round = setupGame(jewelArr)
-
-    $("#comp-num").text(round.compNum)
-
-    $("#player-num").text(round.playerNum)
-
-    $("#wins").text(wins)
-
-    $("#losses").text(losses)
+  setupGame(jewelArr)
+  wins = 0;
+  losses = 0;
+  $("#wins, #losses").empty();
 });
-
-// var round = setupRound(jewelArr)
-
-    // var wins = 0
-    // var losses = 0
-    // var compNum = largeRanNum()
-    // var playerNum = 0
-    // var bttnNum = jewelGenerator(jewelArr)
-
-// // sets up game
-// function setupGame(arr, win, losses) {
-//     var newRound = {
-//       compNum: largeRanNum()
-//       wins: win,
-//       losses: losses,
-//       round: setupRound(jewelGenerator(arr)),
-//     }
-//     return newRound
-// }
